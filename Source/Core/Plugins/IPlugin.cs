33b5234d5ff04db9368841d821c2fa03bec62827
Eric J. Smith <eric@codesmithtools.com>
2014-02-11 17:52:07 -0600

    Started working on plugin system.

---
+++ Source/Core/Plugins/IPlugin.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Exceptionless.Models;
+
+namespace Exceptionless.Core.Plugins {
+    public interface IPlugin {
+        void OnErrorStackCreated(PluginContext context, ErrorStack stack, Error error);
+        void OnErrorOccurence(PluginContext context, Error error);
+        void OnErrorStackRegression(PluginContext context, ErrorStack stack, Error error);
+        void OnErrorStackPromoted(PluginContext context, ErrorStack stack);
+        void OnErrorStackFixed(PluginContext context, ErrorStack stack);
+    }
+}
